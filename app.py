6193be8fab4620e53a57ed53fec0f2d606e0d610
KhaledAlrashidi1 <alrashidi.kha@gmail.com>
2024-07-26 19:20:21 +0300

    small changes 1

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
-
+    # app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get port from environment variable
+    app.run(debug=True, host='0.0.0.0', port=port)
     
